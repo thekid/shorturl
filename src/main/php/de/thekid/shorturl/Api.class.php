f2f2f477bb6d0b3e4b711b502b0ba800cad38df1
Timm Friebe <thekid@thekid.de>
2016-08-14 12:40:53 +0200

    Move application wiring to Bindings class

--- src/main/php/de/thekid/shorturl/Api.class.php
+++ src/main/php/de/thekid/shorturl/Api.class.php
@@ -15,30 +15,14 @@ class Api implements \xp\scriptlet\WebLayout {
   public function mappedApplications($profile= null) {
     $injector= new Injector(new Bindings());
 
-    $context= newinstance(RestContext::class, [], [
-      'handlerInstanceFor' => function($class) use($injector) {
-        return $injector->get($class);
-      }
-    ]);
-
     if ('dev' === $profile) {
-      $context->setTrace((new LogCategory('web'))->withAppender(new ConsoleAppender(), LogLevel::WARN | LogLevel::ERROR));
+      $injector->get(RestContext::class)->setTrace((new LogCategory('web'))->withAppender(
+        new ConsoleAppender(),
+        LogLevel::WARN | LogLevel::ERROR)
+      );
     }
 
-    return ['/' => (new WebApplication('default'))
-      ->withScriptlet(RestScriptlet::class)
-      ->withArguments(['de.thekid.shorturl.api', '/', $context])
-      ->withFilter(newinstance(Filter::class, [], [
-        'filter' => function($request, $response, $invocation) use($injector) {
-          if ($injector->get('string', 'credentials') === $request->getEnvValue('PHP_AUTH_USER').':'.$request->getEnvValue('PHP_AUTH_PW')) {
-            $request->addHeader('user', $request->getEnvValue('PHP_AUTH_USER'));
-          } else {
-            $request->addHeader('user', null);
-          }
-          return $invocation->proceed($request, $response);
-        }
-      ]))
-    ]; 
+    return ['/' => $injector->get(WebApplication::class)];
   }
 
   /** @return [:var] */
