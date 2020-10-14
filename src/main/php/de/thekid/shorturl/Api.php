b008b160f8dcf002d3a5b15dd3211407b2dabe33
Timm Friebe <thekid@thekid.de>
2020-10-14 21:55:49 +0200

    Make use of xp-forge/web-auth

--- src/main/php/de/thekid/shorturl/Api.php
+++ src/main/php/de/thekid/shorturl/Api.php
@@ -1,36 +1,19 @@
 <?php namespace de\thekid\shorturl;
 
 use inject\{Injector, ConfiguredBindings};
+use web\Application;
+use web\auth\Basic;
 use web\rest\{RestApi, ResourcesIn};
-use web\{Application, Filters};
 
 class Api extends Application {
-
-  /** Basic authentication filter */
-  private function basicAuth($pass) {
-    $admin= base64_encode('admin:'.$pass);
-    return fn($request, $response, $invocation) => {
-      if (sscanf($request->header('Authorization'), 'Basic %s', $authorization) > 0) {
-        if ($authorization !== $admin) {
-          $response->header('WWW-Authenticate', 'Basic realm="Administration"');
-          $response->answer(401, 'Unauthorized');
-          return;
-        }
-
-        $request->pass('user', 'admin');
-      } else {
-        $request->pass('user', null);
-      }
-      return $invocation->proceed($request, $response);
-    };
-  }
 
   /** Routing */
   public function routes() {
     $injector= new Injector(new ConfiguredBindings($this->environment->properties('inject')));
-    return new Filters(
-      [$this->basicAuth($injector->get('string', 'admin-pass'))],
-      new RestApi(new ResourcesIn('de.thekid.shorturl.api', [$injector, 'get']))
-    );
+    $auth= new Basic('Administration', fn($user, $secret) => {
+      return 'admin' === $user && $secret->equals($injector->get('string', 'admin-pass')) ? 'admin' : null;
+    });
+
+    return $auth->optional(new RestApi(new ResourcesIn('de.thekid.shorturl.api', [$injector, 'get'])));
   }
 }
