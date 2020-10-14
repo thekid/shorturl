6d58ae9ebaf89cfbe4bf289a32b1bece723c4562
Timm Friebe <thekid@thekid.de>
2020-10-14 21:56:51 +0200

    Improve error messages when invoked without authentication

--- src/main/php/de/thekid/shorturl/api/Administration.php
+++ src/main/php/de/thekid/shorturl/api/Administration.php
@@ -1,14 +1,16 @@
 <?php namespace de\thekid\shorturl\api;
 
-use web\Request;
 use web\rest\{Response, Resource, Get, Delete, Value};
+use web\{Request, Error};
 
 #[Resource]
 class Administration extends Handler {
 
   /** Returns all URLs */
   #[Get('/')]
-  public function all(#[Value] string $user, Request $request): Response {
+  public function all(Request $request, #[Value] $user= null): Response {
+    $user ?? throw new Error(403, 'Must be authenticated to list all URLs');
+
     $pagination= $this->paging()->on($request);
     return $pagination->paginate($this->urls->all(
       $pagination->start() ?: 0,
@@ -18,7 +20,9 @@ class Administration extends Handler {
 
   /** Deletes a URL by a given ID */
   #[Delete('/{id}')]
-  public function delete(#[Value] string $user, string $id): Response {
+  public function delete(string $id, #[Value] $user= null): Response {
+    $user ?? throw new Error(403, 'Must be authenticated to delete URLs');
+
     if ($this->urls->remove($id)) {
       return Response::noContent();
     } else {
